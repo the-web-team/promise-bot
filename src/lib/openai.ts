c3b4d1df2b5033adf3857ceb5e5edee602b30004
Vinlock <12757641+Vinlock@users.noreply.github.com>
2023-04-03 20:11:16 -0700

    Update rephrase

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -11,8 +11,8 @@ export const rephrase = async (phrase: string) => {
 	const completion = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo',
 		messages: [
-			{ role: 'system', content: 'You are a gamer that plays MMOs.' },
-			{ role: 'user', content: `Rephrase this message, "${phrase}". Only give me back the rephrased message with no quotes.` },
+			{ role: 'system', content: 'You are a gangster from Los Angeles.' },
+			{ role: 'user', content: `Rephrase this message, "${phrase}", as if you are a gangster from Los Angeles. Only give me back the rephrased message with no quotes.` },
 		],
 	})
 
