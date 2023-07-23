f8662e4bc9de3c8187a6a729ae127007d9561f3b
Dak Washbrook <hey@dak.dev>
2023-07-22 18:32:38 -0700

    logging and updates

--- src/plugins/voiceLogging.ts
+++ src/plugins/voiceLogging.ts
@@ -12,6 +12,15 @@ const voiceLoggerPlugin = createPlugin({
 		const config = await getGuildConfig(oldState.guild.id)
 
 		if (config.plugins?.voiceLogging?.enabled) {
+			console.log('oldState.channelId', oldState.channelId)
+			console.log('newState.channelId', newState.channelId)
+			console.log('oldState.member.displayName', oldState.member?.displayName)
+			console.log('oldState.member.id', oldState.member?.id)
+			console.log('oldState.channel.id', oldState.channel?.id)
+			console.log('newState.member.displayName', newState.member?.displayName)
+			console.log('newState.member.id', newState.member?.id)
+			console.log('newState.channel.id', newState.channel?.id)
+
 			// User leaves a channel
 			if (oldState.channelId === newState.channelId) {
 				if (oldState.channelId && oldState.member && oldState.channel) {
