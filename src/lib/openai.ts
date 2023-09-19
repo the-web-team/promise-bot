43340e14ff43993d4cb9995855b10a9b4feaef60
Dak Washbrook <hey@dak.dev>
2023-09-18 19:33:52 -0700

    Fix openai

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -22,7 +22,7 @@ export const trainedChatCompletion = async (training: string[], message: string)
 	})
 
 	const completion = await openai.chat.completions.create({
-		model: 'gpt-4',
+		model: 'gpt-3.5-turbo',
 		messages: [
 			...trainingMessages,
 			{
